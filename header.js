6858a3dc432459b1da136cd5bcb8e2fa893d004d
theo <theo.cholet@gmail.com>
2018-04-14 10:48:46 +0200

    update css

--- header.js
+++ header.js
@@ -5,8 +5,7 @@
 // @author       Anonymous
 // @match        https://f95zone.com/*
 // @require      https://code.jquery.com/jquery-3.2.1.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/izimodal/1.5.1/js/iziModal.min.js
-// @resource     f95 https://raw.githubusercontent.com/jvc06042017/f95-threads/master/bulma.css
+// @resource     f95 https://raw.githubusercontent.com/jvc06042017/f95-threads/master/bulma.css?v=1
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
